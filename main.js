db40d8842f3bbd5d78db43514a257c16afeac99b
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-02-01 18:17:20 -0800

    do npm install from main.js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-var bamArtifact = require('./artifact');
-
+var bamArtifact;
 if (typeof require !== 'undefined' && require.main === module) {
-    bamArtifact.run(process.argv.slice(2));
+    const { exec } = require('child_process');
+    console.log(`current directory is ${process.cwd()}`)
+    exec("npm install", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        bamArtifact = require('./artifact');
+        bamArtifact.run(process.argv.slice(2));
+    });    
 }
 
 module.exports = bamArtifact;
